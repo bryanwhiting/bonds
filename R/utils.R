a9ea101a18abc5aa6bc2c3ae788a160198e16327
Bryan Whiting <bryan.g.whiting@gmail.com>
2021-10-26 15:29:26 -0700

    Added parquet

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,7 @@
+dir_output <- here::here("output")
+db_path <- here::here(dir_output, "data.db")
+dir_pqt <- here::here(dir_output, "pqt")
+
 #' Extracts integers from filenames
 #'
 #' @param dir directory where files are stored on nfs
@@ -6,6 +10,261 @@
 #' @export
 #'
 #' @examples
-extract_file_idx <- function(dir){
+extract_file_idx <- function(dir = "/home/rstudio/data/data-challenge/data/interconnect_json/") {
+  dir_ls(dir) %>%
+    basename() %>%
+    str_replace("file_(.*)\\.json", "\\1") %>%
+    as.numeric() %>%
+    sort() %>%
+    return()
+}
 
+create_filename <- function(idx, root = "/home/rstudio/data/data-challenge/data/interconnect_json/") {
+  path(root, glue("file_{idx}"), ext = "json") %>%
+    return()
+}
+
+load_file_as_tibble <- function(json_file) {
+  jsonlite::fromJSON(json_file) %>%
+    as_tibble() %>%
+    janitor::clean_names() %>%
+    return()
+}
+
+#' Parse and Clean Data
+#'
+#' The raw data is a nested dataframe. The command "unnest"
+#' will take the arrays and flatten them, repeating each
+#' row of the parent.
+#'
+#' I create three dataframes here, to make it easy to
+#' save them out to SQL. I retain the file_id in the bonds table
+#' and the bond_id in the sample table to make it easy to join.
+#'
+#' @param df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+parse_and_clean_data <- function(df) {
+  # Clean the raw data
+  # unnest will repeat values for every row in array $bonds
+  # selecting file_id enables me to retain a primary key
+
+  df_bonds <- df %>%
+    select(file_id, bonds) %>%
+    unnest(bonds) %>%
+    janitor::clean_names()
+
+  # Create table of samples with file_id and bond_id for joins
+  df_samples_final <- df_bonds %>%
+    select(file_id, bond_id, trace_samples) %>%
+    unnest(trace_samples) %>%
+    janitor::clean_names()
+
+  # create a bonds table by dropping the trace_samples
+  df_bonds_final <- df_bonds %>%
+    select(-trace_samples) %>%
+    mutate(date_time = lubridate::ymd_hms(date_time))
+
+
+  df_file_final <- df %>%
+    select(-bonds) %>%
+    distinct() %>%
+    mutate(start_date_time = lubridate::ymd_hms(start_date_time))
+
+  return(
+    list(
+      files = df_file_final,
+      bonds = df_bonds_final,
+      samples = df_samples_final
+    )
+  )
+}
+
+
+#' Loads the three data sets into a sqlite database
+#'
+#' @param data List of dataframes
+#' @param db_path database path
+#'
+#' @return
+#' @export
+#'
+#' @examples
+load_to_sqlite <- function(data, db = db_path, idx = NULL) {
+  # con <- dbConnect(RSQLite::SQLite(), db=db_path)
+  pool <- dbPool(RSQLite::SQLite(), db = db)
+  # Initialize the schema
+  created_tables <- dbListTables(conn = pool)
+  for (tab in c("files", "bonds", "samples")) {
+    if (tab %in% created_tables) {
+      add_column_if_doesnt_exist(df = data[[tab]], pool = pool, tab = tab)
+      logger::log_info("Updating [data.", tab, "] from file {idx}")
+      dbAppendTable(conn = pool, name = tab, data[[tab]])
+    } else {
+      dbWriteTable(conn = pool, name = tab, data[[tab]])
+    }
+  }
+  # dbDisconnect(con)
+  poolClose(pool)
+}
+
+#' Checks if every column in the dataset exists in the
+#' SQLITE table. If it doesn't, it adds one.
+#'
+#' @param data List of dataframes
+#' @param pool Connection
+#' @param table
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_column_if_doesnt_exist <- function(df, pool, tab) {
+  # debugging
+  # df <- data[[tab]]
+  # cols <- colnames(df)
+  # tab = 'bonds'
+
+  # List columns in sqlite table
+  # https://stackoverflow.com/a/948204
+  db_columns <- dbGetQuery(pool, glue("PRAGMA table_info({tab})")) %>%
+    pull(name)
+
+  for (c in colnames(df)) {
+    if (!(c %in% db_columns)) {
+      # What value should it be in SQLite?
+      # dbDataType returns the sqlite value from the R object type
+      db_data_type <- dbDataType(pool, df[[c]])
+      logger::log_info("Adding ", c, " AS ", db_data_type, " to [data.", tab, "]")
+
+      # Add new column
+      # https://stackoverflow.com/a/50893477
+      query <- glue("ALTER TABLE {tab} ADD COLUMN {c} {db_data_type};")
+      dbExecute(pool, query)
+    }
+    # TODO: Ensure column was added
+    # new_cols <- dbGetQuery(pool, glue('PRAGMA table_info({tab})')) %>%
+    #   pull(name)
+    # msg = glue('Column {c} was NOT added.') %>% as.character()
+    # stopifnot(c %in% new_cols)
+  }
+}
+
+#' Write as parquet
+#'
+#' Creates individual tables for each section of json object
+#'
+#' @param data
+#' @param idx
+#'
+#' @return
+#' @export
+#'
+#' @examples
+save_as_parquet <- function(data, idx) {
+  tabs <- names(data)
+  # Loop over tab names and save out as pqt/bonds/00001.parquet
+  purrr::map(tabs, ~ {
+    fp <- here::here(dir_pqt, ., glue("{str_pad(idx,5,pad=0)}.parquet"))
+    fs::dir_create(path_dir(fp))
+    arrow::write_parquet(data[[.]], fp)
+  })
+}
+
+#' Run the entire ETL process for one file index
+#'
+#' Assumes files have format /././file_<idx>.json
+#'
+#' @param idx
+#'
+#' @return
+#' @export
+#'
+#' @examples
+etl_one_file <- function(idx) {
+  # all steps (process all files)
+  fp <- create_filename(idx = idx)
+  df <- load_file_as_tibble(fp)
+  data <- parse_and_clean_data(df)
+  load_to_sqlite(data, idx = idx)
+  save_as_parquet(data, idx = idx)
+}
+
+
+
+#' Process all files in the given folder
+#'
+#' @return data.db file with three tables
+#' @export
+#'
+#' @examples
+etl_all_files <- function() {
+  idxs <- extract_file_idx()
+  for (i in idxs) {
+    print(i)
+    etl_one_file(i)
+  }
+  # purrr::map(idxs, etl_one_file)
+
+  # TODO: parallelize
+  # future::plan(future::multisession, workers = 3)
+  # furrr::future_map(idxs,
+  #                   ~etl_all_files,
+  #                   .options = furrr::furrr_options(globals='etl_all_files') )
+}
+
+db_to_fst <- function(db_path = db_path) {
+  pool <- dbPool(RSQLite::SQLite(), db = db_path)
+
+  x <- dbGetQuery(pool, "
+    select *
+    from samples
+    left join bonds using(bond_id)
+    left join files using(file_id)
+    where
+    samples.file_id = 1
+    AND files.file_id = 1
+    AND bonds.file_id = 1
+  ")
+}
+
+#' Read in a multipart parquet file using a given file range
+#'
+#' @param tab
+#' @param range
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' read_parquet_table("files", range = 1:5)
+read_parquet_table <- function(tab, range = NULL) {
+  dir_pqt <- here::here(dir_pqt, tab)
+  file_list <- dir_ls(dir_pqt)
+  if (!is.null(range)) {
+    part_files <- str_pad(range, 5, pad = "0")
+    file_list <- glue("{dir_pqt}/{part_files}.parquet")
+  }
+  # read in multipart parquet
+  # https://stackoverflow.com/a/58450079
+  df <- data.table::rbindlist(
+    lapply(
+      file_list,
+      function(x) arrow::read_parquet(x)
+    ),
+    fill = TRUE
+  )
+  return(as_tibble(df))
+}
+
+# TODO: create automl tool?
+prep_for_ml <- function(db_path = db_path) {
+  pool <- dbPool(RSQLite::SQLite(), db = db_path)
+  dbGetQuery(
+    pool,
+    "select"
+  )
 }
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,2 +1,15 @@
 test_that("extract_filenames() works", {
+  expect_equal(extract_file_idx(), 1:15)
+})
+
+test_that("create_filename() works", {
+  ans <-  "/home/rstudio/data/data-challenge/data/interconnect_json/file_3.json"
+  obs <- as.character(create_filename(idx=3))
+  expect_equal(obs, ans)
+})
+
+test_that("read_parquet_table() works", {
+  x <- read_parquet_table('files', range=4:6)
+  expect_true(min(x$file_id) == 4)
+  expect_true(max(x$file_id) == 6)
 })
